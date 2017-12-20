e0855a6dcee692532bb072e690fd14c78461aac5
ljbreak2008 <ljbreak2008@163.com>
2017-12-20 15:18:57 +0800

    fix bug

--- tests/OSS/Tests/OssClientBucketStorageCapacityTest.php
+++ tests/OSS/Tests/OssClientBucketStorageCapacityTest.php
@@ -15,13 +15,13 @@ class OssClientBucketStorageCapacityTest extends TestOssClientBase
         } catch (OssException $e) {
             $this->assertTrue(false);
         }
-        
+
         try {
             $this->ossClient->putBucketStorageCapacity($this->bucket, 1000);
         } catch (OssException $e) {
             $this->assertTrue(false);
         }
-        
+
         try {
             Common::waitMetaSync();
             $storageCapacity = $this->ossClient->getBucketStorageCapacity($this->bucket);
@@ -29,12 +29,29 @@ class OssClientBucketStorageCapacityTest extends TestOssClientBase
         } catch (OssException $e) {
             $this->assertTrue(false);
         }
-        
+
+        try {
+            $this->ossClient->putBucketStorageCapacity($this->bucket, 0);
+
+            Common::waitMetaSync();
+
+            $storageCapacity = $this->ossClient->getBucketStorageCapacity($this->bucket);
+            $this->assertEquals($storageCapacity, 0);
+
+            $this->ossClient->putObject($this->bucket, 'test-storage-capacity','test-content');
+            $this->assertTrue(false);
+        } catch (OssException $e) {
+            $this->assertEquals('Bucket storage exceed max storage capacity.',$e->getErrorMessage());
+        }
+
         try {
             $this->ossClient->putBucketStorageCapacity($this->bucket, - 2);
             $this->assertTrue(false);
         } catch (OssException $e) {
+            $this->assertEquals(400, $e->getHTTPStatus());
             $this->assertEquals('InvalidArgument', $e->getErrorCode());
         }
+
+
     }
 }
