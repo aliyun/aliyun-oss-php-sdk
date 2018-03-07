a2176492013642586559a3785663f48a5b0d741d
ljbreak2008 <ljbreak2008@163.com>
2018-03-07 15:00:49 +0800

    test

--- tests/OSS/Tests/OssClientTest.php
+++ tests/OSS/Tests/OssClientTest.php
@@ -213,21 +213,15 @@ class OssClientTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(array_key_exists('via', $result));
     }
 
-    //获取唯一序列号
-    private  function generateNum()
-    {
-        $charid = strtolower(md5(uniqid(mt_rand(), true)));
-        $uuid = substr($charid, 0, 8).substr($charid, 8, 4).substr($charid,12, 4).substr($charid,16, 4).substr($charid,20,12);
-        return $uuid;
-    }
-
     public static function setUpBeforeClass()
     {
         $accessKeyId = ' ' . getenv('OSS_ACCESS_KEY_ID') . ' ';
         $accessKeySecret = ' ' . getenv('OSS_ACCESS_KEY_SECRET') . ' ';
         $endpoint = ' ' . getenv('OSS_ENDPOINT') . '/ ';
         $ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint, false);
-        $bucket = (new self())->generateNum() . '-' . getenv('OSS_BUCKET');
+        $charid = strtolower(md5(uniqid(mt_rand(), true)));
+        $uuid = substr($charid, 0, 8).substr($charid, 8, 4).substr($charid,12, 4).substr($charid,16, 4).substr($charid,20,12);
+        $bucket = $uuid . '-' . getenv('OSS_BUCKET');
         putenv('OSS_BUCKET =' . $bucket);
         $ossClient ->createBucket($bucket);
     }
@@ -239,6 +233,15 @@ class OssClientTest extends \PHPUnit_Framework_TestCase
         $endpoint = ' ' . getenv('OSS_ENDPOINT') . '/ ';
         $ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint, false);
         $bucket = getenv('OSS_BUCKET');
+        $listObjectInfo = $ossClient->listObjects($bucket);
+        $listObject = $listObjectInfo->getObjectList();
+        if(count($listObject) != 0){
+            foreach($listObject as $object){
+                $fileName = $object->getkey();
+                $ossClient->deleteObject($bucket,$fileName);
+
+            }
+        }
         $ossClient ->deleteBucket($bucket);
     }
 }
