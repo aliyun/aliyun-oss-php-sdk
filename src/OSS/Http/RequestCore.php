42142bb4314685757ca64136cf9ed7a100380658
晟章 <shengzhang.wsz@antgroup.com>
2020-12-23 10:08:43 +0800

    compatible with PHP8

--- src/OSS/Http/RequestCore.php
+++ src/OSS/Http/RequestCore.php
@@ -789,7 +789,7 @@ class RequestCore
         }
 
         // As long as this came back as a valid resource...
-        if (is_resource($curl_handle)) {
+        if ($curl_handle !== false) {
             // Determine what's what.
             $header_size = curl_getinfo($curl_handle, CURLINFO_HEADER_SIZE);
             $this->response_headers = substr($this->response, 0, $header_size);
