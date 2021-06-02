ffa803305425918595bc297a5172cc550ad0036c
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2021-06-02 14:37:07 +0800

    Compatible with PHP8, remove (string)$curl_handle.

--- src/OSS/Http/RequestCore.php
+++ src/OSS/Http/RequestCore.php
@@ -844,7 +844,7 @@ class RequestCore
         $this->response = curl_exec($curl_handle);
 
         if ($this->response === false) {
-            throw new RequestCore_Exception('cURL resource: ' . (string)$curl_handle . '; cURL error: ' . curl_error($curl_handle) . ' (' . curl_errno($curl_handle) . ')');
+            throw new RequestCore_Exception('cURL error: ' . curl_error($curl_handle) . ' (' . curl_errno($curl_handle) . ')');
         }
 
         $parsed_response = $this->process_response($curl_handle, $this->response);
