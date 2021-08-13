15209504781b98890a1ffa128255c9a37bac185a
yangpeng <183851063@qq.com>
2021-08-13 16:46:05 +0800

    Lifecycle adds multiple tags, Transition, and NoncurrentVersionTransition

--- src/OSS/Model/LifecycleRule.php
+++ src/OSS/Model/LifecycleRule.php
@@ -85,6 +85,284 @@ class LifecycleRule
     {
         $this->actions = $actions;
     }
+	
+	/**
+	 * @return bool
+	 */
+    public function hasTags(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Tag'){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function hasExpirationDays(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Expiration' && $action->getTimeSpec() == 'Days'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public function hasAbortMultipartUpload(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'AbortMultipartUpload'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public function hasAbortMultipartUploadExpirationDays(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'AbortMultipartUpload' && $action->getTimeSpec() == 'Days'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public function hasAbortMultipartUploadCreatedBeforeDate(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'AbortMultipartUpload' && $action->getTimeSpec() == 'CreatedBeforeDate'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function hasCreatedBeforeDate(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Expiration' && $action->getTimeSpec() == 'CreatedBeforeDate'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function hasExpiredDeleteMarker(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Expiration' && $action->getTimeSpec() == 'ExpiredObjectDeleteMarker'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function hasStorageTransition(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Transition'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	/**
+	 * @return bool
+	 */
+	public function hasStorageTransitionExpirationDays(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Transition' && $action->getTimeSpec() == 'Days'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function hasStorageTransitionCreatedBeforeDate(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Transition' && $action->getTimeSpec() == 'CreatedBeforeDate'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function hasNoncurrentVersionStorageTransitions(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'NoncurrentVersionTransition'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function hasNoncurrentVersionExpiration(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'NoncurrentVersionExpiration'){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getTags(){
+    	$str = '';
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Tag'){
+				switch ($action->getTimeSpec()){
+					case 'Key':
+						$str .= '{'.$action->getTimeValue();
+						break;
+					case 'Value':
+						$str .= '='.$action->getTimeValue().'}';
+						break;
+				}
+			}
+		}
+		return $str;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getExpirationDays(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Expiration' && $action->getTimeSpec() == 'Days'){
+				return $action->getTimeValue();
+			}
+		}
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getCreatedBeforeDate(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Expiration' && $action->getTimeSpec() == 'CreatedBeforeDate'){
+				return $action->getTimeValue();
+			}
+		}
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getAbortMultipartUploadExpirationDays(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'AbortMultipartUpload' && $action->getTimeSpec() == 'Days'){
+				return $action->getTimeValue();
+			}
+		}
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getAbortMultipartUploadCreatedBeforeDate(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'AbortMultipartUpload' && $action->getTimeSpec() == 'CreatedBeforeDate'){
+				return $action->getTimeValue();
+			}
+		}
+	}
+	
+	
+	
+	public function getStorageTransitionExpirationDays(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Transition' &&  $action->getTimeSpec() == 'Days'){
+				return $action->getTimeValue();
+			}
+		}
+	}
+	
+	public function getStorageTransitionCreatedBeforeDate(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Transition' &&  $action->getTimeSpec() == 'CreatedBeforeDate'){
+				return $action->getTimeValue();
+			}
+		}
+	}
+	
+	public function getStorageTransitionStorageClass(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Transition' &&  $action->getTimeSpec() == 'StorageClass'){
+				return $action->getTimeValue();
+			}
+		}
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getExpiredDeleteMarker(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'Expiration' && $action->getTimeSpec() == 'ExpiredObjectDeleteMarker'){
+				return $action->getTimeValue();
+			}
+		}
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getNoncurrentVersionStorageTransitionsNoncurrentDays(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'NoncurrentVersionTransition' && $action->getTimeSpec() == 'NoncurrentDays'){
+				return  $action->getTimeValue();
+			}
+			
+		}
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getNoncurrentVersionStorageTransitionsStorageClass(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'NoncurrentVersionTransition' && $action->getTimeSpec() == 'StorageClass'){
+				return  $action->getTimeValue();
+			}
+			
+		}
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getNoncurrentVersionExpirationDays(){
+		foreach ($this->actions as $action) {
+			if($action->getAction() == 'NoncurrentVersionExpiration'){
+				switch ($action->getTimeSpec()){
+					case 'NoncurrentDays':
+						return $action->getTimeValue();
+				}
+			}
+		}
+	}
 
 
     /**
