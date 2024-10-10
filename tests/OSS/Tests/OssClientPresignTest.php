a0e16ba4425f989838400fe3161a8966e8421210
yangpeng <yp01106192@alibaba-inc.com>
2024-10-10 15:33:29 +0800

    refine generate query string funtion code

---
+++ tests/OSS/Tests/OssClientPresignTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace OSS\Tests;
+
+use OSS\Core\OssException;
+use OSS\Credentials\StaticCredentialsProvider;
+use OSS\Http\RequestCore;
+use OSS\Http\ResponseCore;
+use OSS\OssClient;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'TestOssClientBase.php';
+
+
+class OssClientPresignTest extends TestOssClientBase
+{
+    protected $stsOssClient;
+
+    public function testObjectWithSignV1()
+    {
+        $config = array(
+            'signatureVersion' => OssClient::OSS_SIGNATURE_VERSION_V1
+        );
+        $this->bucket = Common::getBucketName() . '-' . time();
+        $this->ossClient = Common::getOssClient($config);
+        $this->ossClient->createBucket($this->bucket);
+        Common::waitMetaSync();
+
+        $object = "a.file";
+        $this->ossClient->putObject($this->bucket, $object, "hi oss");
+        $timeout = 3600;
+        $options = array(
+            "response-content-disposition" => "inline"
+        );
+        try {
+            $signedUrl = $this->ossClient->signUrl($this->bucket, $object, $timeout, OssClient::OSS_HTTP_GET, $options);
+        } catch (OssException $e) {
+            $this->assertFalse(true);
+        }
+        $this->assertStringContainsString("response-content-disposition=inline", $signedUrl);
+        $options = array(
+            "response-content-disposition" => "attachment"
+        );
+
+        try {
+            $signedUrl = $this->ossClient->signUrl($this->bucket, $object, $timeout, OssClient::OSS_HTTP_GET, $options);
+        } catch (OssException $e) {
+            $this->assertFalse(true);
+        }
+        $this->assertStringContainsString("response-content-disposition=attachment", $signedUrl);
+    }
+
+    public function testObjectWithSignV4()
+    {
+        $config = array(
+            'signatureVersion' => OssClient::OSS_SIGNATURE_VERSION_V4
+        );
+        $this->bucket = Common::getBucketName() . '-' . time();
+        $this->ossClient = Common::getOssClient($config);
+        $this->ossClient->createBucket($this->bucket);
+        Common::waitMetaSync();
+
+        $object = "a.file";
+        $this->ossClient->putObject($this->bucket, $object, "hi oss");
+        $timeout = 3600;
+        $options = array(
+            "response-content-disposition" => "inline"
+        );
+        try {
+            $signedUrl = $this->ossClient->signUrl($this->bucket, $object, $timeout, OssClient::OSS_HTTP_GET, $options);
+        } catch (OssException $e) {
+            $this->assertFalse(true);
+        }
+        $this->assertStringContainsString("response-content-disposition=inline", $signedUrl);
+        $options = array(
+            "response-content-disposition" => "attachment"
+        );
+
+        try {
+            $signedUrl = $this->ossClient->signUrl($this->bucket, $object, $timeout, OssClient::OSS_HTTP_GET, $options);
+        } catch (OssException $e) {
+            $this->assertFalse(true);
+        }
+        $this->assertStringContainsString("response-content-disposition=attachment", $signedUrl);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->ossClient->deleteObject($this->bucket, "a.file");
+        parent::tearDown();
+    }
+
+    protected function setUp(): void
+    {
+    }
+}
